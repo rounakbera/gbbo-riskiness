2ee27ced3993f25916a4f2e24742108a9f61e2ac
riagarg <riagarg@gmail.com>
2020-04-26 02:24:41 -0700

    risk graph

--- article/src/components/BarchartExample.js
+++ article/src/components/BarchartExample.js
@@ -3,7 +3,8 @@ import {
   VictoryBar, 
   VictoryChart, 
   VictoryAxis,
-  VictoryTheme 
+  VictoryTheme,
+  VictoryTooltip
 } from 'victory';
 
 const sampleData = [
@@ -16,9 +17,8 @@ const sampleData = [
 export default class BarchartExample extends React.Component {
   constructor(props) {
     super(props);
-    
     this.state = {
-      data: props.data || sampleData,
+      data: props.data|| sampleData,
       animate: props.animate,
       animating: false
     }
@@ -35,27 +35,67 @@ export default class BarchartExample extends React.Component {
       setTimeout(() => this.setState({animating: false}), this.state.animate);
     }
   }
+  componentDidMount() {
+    this.setStateInterval = window.setInterval(() => {
+      this.setState({
+        data: this.getData()
+      });
+    }, 3000);
+  }
+  
+  componentWillUnmount() {
+    window.clearInterval(this.setStateInterval);
+  }
+
+
+  getData() {
+    return [ {"baker": "Stu", "risk": 1.0, "rank": 12}, {"baker": "Mat", "risk": 0.2857142857142857, "rank": 6}, {"baker": "Nadiya", "risk": 0.65, "rank":1}];
+  
+  }
+
+
 
   render() {
     return (
       <VictoryChart
         animate={{ duration: this.state.animate }}
         theme={VictoryTheme.material}
-        domainPadding={20}
+        domainPadding={1}
       >
         <VictoryAxis
-          tickValues={[1, 2, 3, 4]}
-          tickFormat={["Quarter 1", "Quarter 2", "Quarter 3", "Quarter 4"]}
+         // tickValues={[1, 2, 3, 4, 5, 6, 7, 8 ,9 ,10 ,11, 12]}
+         // tickFormat={ ['Alvin', 'Dorret', 'Flora', 'Ian', 'Mat', 'Nadiya', 'Paul', 'Sandy', 'Tamal', 'Ugné', 'Stu', 'Marie']}
+       
         />
         <VictoryAxis
           dependentAxis
-          domain={[0, 20000]}
-          tickFormat={(x) => (`$${x / 1000}k`)}
+          domain={[0, 1]}
+          tickFormat={(x) => (`${x}`)}
+         
         />
         <VictoryBar
+          //categories= {{ x:['Stu','Marie',  'Dorret', 'Sandy', 'Ugne','Alvin',  'Mat',  'Paul',  'Flora','Tamal','Ian', 'Nadiya'] }}
           data={this.state.data}
-          x="quarter"
-          y="earnings"
+          x="baker"
+          y="risk"
+          z="rank"
+          style={{
+            data: { fill:  "purple" },
+            labels: { fontSize: ({ text }) => text.length > 10 ? 5 : 12 },
+            parent: { border: "1px solid #ccc" }
+          }}
+          labels={({datum}) => `x: ${datum.x}, y: ${datum.y}`}
+          labelComponent={<VictoryTooltip dy={0} centerOffset={{ x: 25 }} />}
+          sortKey= "rank"
+          sortOrder="descending"
+          animate={{
+            onExit: {
+              duration: 1000,
+              before: () => ({
+                _y: 0
+              })
+            }
+          }}
         />
       </VictoryChart>
     )
