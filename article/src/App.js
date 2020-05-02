d4d1bc2665fe5aa3b274321159cefadeb9097b24
chiyo <ttl2132@columbia.edu>
2020-05-02 09:14:34 -0400

    Made scatterplot with tooltip. Todo: fix buggy hovering

--- article/src/App.js
+++ article/src/App.js
@@ -3,6 +3,7 @@ import BarchartExample from './components/BarchartExample.js';
 import FlavorScatterplot from './components/FlavorScatterplot.js';
 import ScrollamaExample from './components/ScrollamaExample.js';
 import TitleCard from './components/TitleCard.js';
+import FinalViz from './components/FinalViz.js';
 
 const data = require('./data/flavorRiskinessToPerformance.json');
 
@@ -15,6 +16,7 @@ function App() {
       <TitleCard/>
       <FlavorScatterplot data={data} />
       <BarchartExample animate={500} />
+      <FinalViz/>
       <ScrollamaExample />
     </div>
   );
