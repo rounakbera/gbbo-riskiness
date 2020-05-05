d44315e2e55043cf22a4722f20921f66a050f18e
stacytao <stacyytao@gmail.com>
2020-05-04 22:40:12 -0500

    Goodbye trigger

--- article/src/components/ScrollamaExample.js
+++ article/src/components/ScrollamaExample.js
@@ -102,7 +102,6 @@ export default class ScrollamaExample extends React.Component {
             progress
             onStepProgress={this.onStepProgress}
             offset={0.33}
-            debug
           >
             {steps.map(value => (
               <Step data={value} key={value}>
