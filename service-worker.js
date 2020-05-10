f1bc019bc87fd64271d986a1247b4a3ca8ab9ef1
stacytao <stacyytao@gmail.com>
2020-05-10 17:46:29 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/gbbo-riskiness/precache-manifest.b66c6cf696b8873c4148dbcb8d209074.js"
+  "/gbbo-riskiness/precache-manifest.457933c2069158b7223123a37e14ace4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/gbbo-riskiness/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
